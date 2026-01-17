2f4e544618dcca430f17a659493f3dc5b94389c4
Lenka Hake <lenka.hake@crowe-peak.nl>
2026-01-17 21:02:35 +0100

    Add assignment 3

---
+++ Assignment3/main.py
@@ -0,0 +1,23 @@
+from config.search_space import param_spec, base_cfg
+from policies.pretrained_policy import load_pretrained_policy
+from envs.highway_env_utils import make_env
+from search.hill_climbing_search import HillClimbingSearch
+from search.random_search import RandomSearch
+
+def main():
+    env_id = "highway-fast-v0"
+    policy = load_pretrained_policy("agents/model")
+    env, defaults = make_env(env_id)
+
+    # search = RandomSearch(env_id, base_cfg, param_spec, policy, defaults)
+    # crashes = search.run_search(n_scenarios=50, seed=11)
+
+    search = HillClimbingSearch(env_id, base_cfg, param_spec, policy, defaults)
+    crashes = search.run_search(n_scenarios=10, seed=11, iterations=50, neighbors_per_iteration=10)
+
+    print(f"✅ Found {len(crashes)} crashes.")
+    #if crashes:
+    #    print(crashes)
+
+if __name__ == "__main__":
+    main()
